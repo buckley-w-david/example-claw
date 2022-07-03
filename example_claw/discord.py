aea96c1bd543a70a576ec11abeafd335003da3a9
David Buckley <david@davidbuckley.ca>
2022-07-03 17:09:33 -0400

    refactor for deployment

---
+++ example_claw/discord.py
@@ -0,0 +1,43 @@
+import random
+from typing import cast, List
+
+from flask_discord_interactions import Discord
+from flask_discord_interactions import discord_types as types
+
+discord = Discord()
+
+with open('prefix.txt', 'r') as f:
+    prefix = [l.strip() for l in f.readlines()]
+
+with open('suffix.txt', 'r') as f:
+    suffix = [l.strip() for l in f.readlines()]
+
+@discord.command("names")
+def names(interaction: types.ChatInteraction) -> types.InteractionResponse:
+    names = []
+
+    if (options := interaction.data.options):
+        value = int(options[0].value) # type: ignore
+    else:
+        value = 5
+
+    for _ in range(value):
+        names.append(f"{random.choice(prefix).capitalize()}{random.choice(suffix).lower()}")
+
+    return types.InteractionResponse(
+        type=types.InteractionCallbackType.CHANNEL_MESSAGE_WITH_SOURCE,
+        data=types.InteractionCallbackDataMessages(
+            content="\n".join(names),
+        )
+    )
+names.description = "Generate names"
+
+arg = types.ApplicationCommandOption(
+    type=types.ApplicationCommandOptionType.INTEGER,
+    name="number",
+    description="The number of names to generate",
+    required=False,
+    min_value=1,
+    max_value=10,
+)
+names.add_option(arg)
---
+++ wsgi.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from werkzeug.middleware.proxy_fix import ProxyFix
+
+def init_app(app: Flask):
+    app.wsgi_app = ProxyFix(
+        app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
+)
+
+from example_claw import create_app
+app = create_app()
+init_app(app)
